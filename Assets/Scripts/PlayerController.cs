2f4305cbe568e488e17fd136d52323f217011e81
JohnItalo05 <simonebersani2005@gmail.com>
2025-10-17 13:02:06 +0200

    Completed mission 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 15.0f; // meters per second
+    public float turnSpeed = 45.0f; // degres per second
+    private float verticalInput;
+    private float horizontalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward   
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // Read the input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
